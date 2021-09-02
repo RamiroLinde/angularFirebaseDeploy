b1214b7933f0ee3f22a69c77fb25dad68030152e
Ramiro Linde Rodríguez <55402909+RamiroLinde@users.noreply.github.com>
2021-09-02 13:58:00 +0200

    servicio con los metodos de getAll getById y create para firebase

--- src/app/services/house.service.ts
+++ src/app/services/house.service.ts
@@ -1,31 +1,55 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { House } from '../interfaces/house.interface';
-import { HOUSES } from '../interfaces/houses.db';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HouseService {
 
-  arrHouses: House[];
+  arrHouses: House[] = [];
+  collection!: AngularFirestoreCollection<House>;
   id: number = 5;
-  constructor() { 
-    this.arrHouses = HOUSES;
+  constructor(private firestore: AngularFirestore) {
+    this.collection = this.firestore.collection<House>('houses');
   }
 
-  getAll(){
-    return this.arrHouses;
+  getAll(): Observable<any[]> {
+    return this.collection.valueChanges();
   }
 
-  getBtId(id: number): House | undefined{
-    return this.arrHouses.find(house => house.id === id);
+  getById(id: string){
+    return this.collection.doc(id).valueChanges();
   }
 
-  create(pHouse: House): string {
-    pHouse.id = this.id;
-    this.arrHouses.push(pHouse);
-    this.id++;
-    console.log(this.arrHouses);
-    return 'success';
+  create(pHouse: House): Promise<void> {
+    return new Promise((resolve, reject)=>{
+      try{
+        const id = this.firestore.createId();
+        pHouse.id = id;
+        const result = this.collection.doc(id).set(pHouse);
+        resolve(result);
+      }catch(err){
+        reject(err);
+      }
+    })
+  
   }
+  // getAll(){
+  //   return this.arrHouses;
+  // }
+
+  // getBtId(id: number): House | undefined{
+  //   return this.arrHouses.find(house => house.id === id);
+  // }
+
+  // create(pHouse: House): string {
+  //   // pHouse.id = this.id;
+  //   // this.arrHouses.push(pHouse);
+  //   // this.id++;
+  //   // console.log(this.arrHouses);
+  //   // return 'success';
+  // }
 }
